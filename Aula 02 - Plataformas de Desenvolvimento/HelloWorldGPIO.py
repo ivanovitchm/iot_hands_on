f310921eae804896d8df72c49794768f6b000312
Ivanovitch Silva <ivan@imd.ufrn.br>
2017-08-20 10:58:49 -0300

    Criando o repositório

---
+++ Aula 02 - Plataformas de Desenvolvimento/HelloWorldGPIO.py
@@ -0,0 +1,13 @@
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+for i in range(4):
+	GPIO.setup("USR%d" % i, GPIO.OUT)
+
+while true:
+	for i in range(4):
+		GPIO.output("USR%d" % i, GPIO.HIGH)
+		time.sleep(1)
+	for i in range(4):
+		GPIO.output("USR%d" % i, GPIO.LOW)
+		time.sleep(1)
